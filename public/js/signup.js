9496f8fc4516be965dee203aa6de7307c3ce35e2
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-02-22 21:07:14 -0600

    Messed up, commiting so that i can reset to previous code.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
+  const first_nameInput = $("input#first_name-input");
+  const last_nameInput = $("input#last_name-input")
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
 
@@ -8,15 +10,19 @@ $(document).ready(() => {
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
+      first_name: first_nameInput.val().trim(),
+      last_name: last_nameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.first_name || !userData.last_name || !userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.first_name, userData.last_name, userData.email, userData.password);
+    first_nameInput.val("");
+    last_nameInput.val("");
     emailInput.val("");
     passwordInput.val("");
   });
@@ -25,6 +31,8 @@ $(document).ready(() => {
   // Otherwise we log any errors
   function signUpUser(email, password) {
     $.post("/api/signup", {
+      first_name: first_name,
+      last_name: last_name,
       email: email,
       password: password
     })
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,6 +9,8 @@ module.exports = function(app) {
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
+      first_name: req.user.first_name,
+      last_name: req.user.last_name,
       email: req.user.email,
       id: req.user.id
     });
@@ -18,18 +20,19 @@ module.exports = function(app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
-    db.User.create({
-      first_name: req.body.first_name,
-      last_name: req.body.first_name,
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(() => {
-        res.redirect(307, "/api/login");
-      })
-      .catch(err => {
-        res.status(401).json(err);
-      });
+    console.log(req.body)
+    // db.User.create({
+    //   first_name: req.body.first_name,
+    //   last_name: req.body.first_name,
+    //   email: req.body.email,
+    //   password: req.body.password
+    // })
+    //   .then(() => {
+    //     res.redirect(307, "/api/login");
+    //   })
+    //   .catch(err => {
+    //     res.status(401).json(err);
+    //   });
   });
 
   // Route for logging user out
