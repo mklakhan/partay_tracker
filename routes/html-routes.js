71ca14189a656751f9045bf196b3d157efdcf77a
Pete-Scale <petescale@gmail.com>
2021-02-25 17:59:43 -0600

    updated comment

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,7 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+
+// Requiring our models to get data from the database
 const db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
