e486bbf05974434f237f6dd1375ee71eb7bb75cb
Pete-Scale <petescale@gmail.com>
2021-02-25 18:18:51 -0600

    console log req.user

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,6 +27,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/home", isAuthenticated, (req, res) => {
+    console.log(req.user)
     res.render('home', {
       user: req.user
     })
