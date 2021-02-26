b36ecca54834a8352e4ba7703fd566881a316a2d
Pete-Scale <petescale@gmail.com>
2021-02-26 10:01:01 -0600

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,23 @@ CREATE DATABASE partay_tracker;
 
 USE partay_tracker; 
 
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  user_password VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE partays (
   id INT NOT NULL AUTO_INCREMENT,
-  date DATE NOT NULL,
-  time TIME NOT NULL, 
+  partay_name VARCHAR(255) NOT NULL,
+  partay_summary VARCHAR(255) NOT NULL,
+  partay_date DATE NOT NULL,
+  partay_time TIME NOT NULL, 
+  partay_location VARCHAR(255) NOT NULL,
   host_user_id INT NOT NULL,
-  partay_name VARCHAR(100) NOT NULL,
-  partay_summary VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -22,13 +32,5 @@ response VARCHAR(255),
 PRIMARY KEY (id)
 );
 
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  user_password VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
-);
 
  
