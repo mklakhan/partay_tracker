209e55fa00d0d246b47721b7ef7f16d8f04a13c5
Pete-Scale <petescale@gmail.com>
2021-03-01 14:02:09 -0600

    formatting

--- public/js/home.js
+++ public/js/home.js
@@ -1,20 +1,17 @@
 $(document).ready(() => {
-    // This file just does a GET request to figure out which user is logged in
+
     const attendBtn = $(".attend");
-    // and updates the HTML on the page
+
     $.get("/api/user_data").then(data => {
         $(".member-name").text((data.first_name) + " " + (data.last_name));
     });
 
     attendBtn.on("click", event => {
-        // event.preventDefault();
         const attendData = {
             attending: true,
             partay_id: event.target.value
-            // user_id: user.id
         };
-
-        // attendPartay(attendData.attending, attendData.partay_id, attendData.user_id);
+        
         attendPartay(attendData.attending, attendData.partay_id);
     });
 
